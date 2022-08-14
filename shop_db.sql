7eef80265e08b134fa49445aad6d4f6a2b0b5d4f
rabidbunny50 <rabidbunny50@gmail.com>
2022-08-14 22:15:38 +0530

    changed the sql file to teh updated file

--- shop_db.sql
+++ shop_db.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 17, 2022 at 07:21 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Host: sql102.epizy.com
+-- Generation Time: Aug 14, 2022 at 12:40 PM
+-- Server version: 10.3.27-MariaDB
+-- PHP Version: 7.2.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `shop_db`
+-- Database: `epiz_31748184_shop_db`
 --
 
 -- --------------------------------------------------------
@@ -36,6 +37,14 @@ CREATE TABLE `cart` (
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `name`, `price`, `quantity`, `image`) VALUES
+(80, 7, 'Sin Eater', 100, 1, 'WhatsApp Image 2022-05-18 at 12.15.02 PM.jpeg'),
+(81, 7, 'Six of Crows', 150, 1, 'WhatsApp Image 2022-05-18 at 12.16.57 PM.jpeg');
+
 -- --------------------------------------------------------
 
 --
@@ -50,6 +59,13 @@ CREATE TABLE `message` (
   `number` varchar(12) NOT NULL,
   `message` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`id`, `user_id`, `name`, `email`, `number`, `message`) VALUES
+(12, 11, 'wolverine', 'rrd@gmail.com', '1236547893', 'very nice site');
 
 -- --------------------------------------------------------
 
@@ -71,6 +87,13 @@ CREATE TABLE `orders` (
   `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`) VALUES
+(20, 11, 'Sandeep', '123', '123@gmail.com', 'credit card', 'flat no. 581320, Sgah, Bhatkal, India - 581320', ', The Road (1) ', 300, '14-Aug-2022', 'pending');
+
 -- --------------------------------------------------------
 
 --
@@ -84,6 +107,25 @@ CREATE TABLE `products` (
   `description` varchar(255) NOT NULL,
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `description`, `image`) VALUES
+(9, 'Sin Eater', 100, 'For the crime of stealing bread, fourteen-year-old May receives a life sentence: she must become a Sin Eater—a shunned woman, brutally marked, whose fate is to hear the final confessions of the dying, eat ritual foods symbolizing their sins as a funeral r', 'WhatsApp Image 2022-05-18 at 12.15.02 PM.jpeg'),
+(10, 'Six of Crows', 150, 'Six of Crows is the story of Kaz Brekker and his crew, attempting to pull off an impossible heist. There is Kaz, known as \'Dirtyhands\' in the Barrel (the slums of Ketterdam), who is part of The Dregs. The Dregs is a gang, and as the name suggests consists', 'WhatsApp Image 2022-05-18 at 12.16.57 PM.jpeg'),
+(11, 'The Road', 300, 'The Road is a 2006 post-apocalyptic novel by American writer Cormac McCarthy. The book details the grueling journey of a father and his young son over a period of several months across a landscape blasted by an unspecified cataclysm that has destroyed ind', 'WhatsApp Image 2022-05-18 at 12.16.03 PM.jpeg'),
+(12, 'A Million Little Pieces', 270, 'A Million Little Pieces is a book by James Frey, originally sold as a memoir and later marketed as a semi-fictional novel following accusations of literary forgery. It tells the story of a 23-year-old alcoholic and abuser of other drugs and how he copes w', 'WhatsApp Image 2022-05-18 at 12.16.30 PM.jpeg'),
+(16, 'The Shy Creatures ', 499, '', 'images (8).jpeg'),
+(17, 'Rapunzel ', 399, '', 'images (2).jpeg'),
+(18, 'Unleash The Dragon ', 299, '', 'images (1).jpeg'),
+(19, 'A Million To One', 799, '', 'images.jpeg'),
+(20, 'The Great Gatsby', 599, '', 'images (4).jpeg'),
+(21, 'A Kite For Moon', 899, '', 'images (7).jpeg'),
+(22, 'Every Thing You Never Had', 299, '', 'images (5).jpeg'),
+(23, 'I Can Make This Promise ', 699, '', 'images (6).jpeg'),
+(24, 'Kgf', 333, '', 'Screenshot_20220813-215417_Quickstep.png');
 
 -- --------------------------------------------------------
 
@@ -104,12 +146,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_type`) VALUES
-(1, 'admin', 'admin@admin.com', '202cb962ac59075b964b07152d234b70', 'admin'),
-(2, 'user', 'user@user.com', '202cb962ac59075b964b07152d234b70', 'user'),
-(3, 'ranjit', 'wer123@gmail.com', '202cb962ac59075b964b07152d234b70', 'user'),
-(4, 'ken', 'ssdh@gjsji.com', '4b1a59c2bb8052dc7e1714caac20d432', 'user'),
-(5, 'ken', 'bri@gmail.com', 'ed2b1f468c5f915f3f1cf75d7068baae', 'user'),
-(6, 'arhhab', 'ar123@gnmad.com', '81dc9bdb52d04dc20036dbd8313ed055', 'user');
+(10, 'Admin', 'admin@admin.com', '202cb962ac59075b964b07152d234b70', 'admin'),
+(11, 'User', 'user@user.com', '81dc9bdb52d04dc20036dbd8313ed055', 'user');
 
 --
 -- Indexes for dumped tables
@@ -153,31 +191,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
